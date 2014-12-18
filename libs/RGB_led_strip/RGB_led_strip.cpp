63fb95c7e00161980b95c9aa520a2a33cab22a24
PaVel <vel.pavel@gmail.com>
2014-12-18 12:56:06 +0300

    Add RGB Arduino lib

---
+++ libs/RGB_led_strip/RGB_led_strip.cpp
@@ -0,0 +1,46 @@
+/*
+  RGB_led_stpip.h - Library for control RGB strip.
+  Created by PaVel, December, 2014.
+*/
+#include "RGB_led_strip.h"
+
+RGB_strip::RGB_strip(byte pinR, byte pinG, byte pinB)
+{
+    pinMode(pinR, OUTPUT);
+    pinMode(pinG, OUTPUT);
+    pinMode(pinB, OUTPUT);
+    _pinR=pinR;
+    _pinG=pinG;
+    _pinB=pinB;
+    //Init values. After it user can use all fuctions without errors
+    _brightness=255; //max brightness
+    _R_color=255;    //white color 
+    _G_color=255;
+    _B_color=255;
+}
+
+void RGB_strip::led_light()
+{
+    analogWrite(_pinR, _R_color*_brightness/255);
+    analogWrite(_pinG, _G_color*_brightness/255);
+    analogWrite(_pinB, _B_color*_brightness/255);
+}
+
+void RGB_strip::set_color(byte R_color, byte G_color, byte B_color)
+{
+    _R_color=R_color;
+    _G_color=G_color;
+    _B_color=B_color;
+    led_light();
+}
+
+void RGB_strip::set_brightness(byte brightness)
+{
+    _brightness=brightness;
+    led_light();
+}
+
+byte RGB_strip::show_brightness()
+{
+    return _brightness;
+}
---
+++ libs/RGB_led_strip/RGB_led_strip.h
@@ -0,0 +1,32 @@
+/*
+  RGB_led_stpip.h - Library for control RGB strip.
+  Created by PaVel, December, 2014.
+*/
+#ifndef Morse_h
+#define Morse_h
+ 
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif 
+
+class RGB_strip
+{
+  public:
+    RGB_strip(byte pinR, byte pinG, byte pinB);
+    void set_color(byte R_color, byte G_color, byte B_color);
+    void set_brightness(byte brightness);
+    byte show_brightness();
+  private:
+    byte _pinR;
+    byte _pinG;
+    byte _pinB;
+    byte _R_color;
+    byte _G_color;
+    byte _B_color;
+    byte _brightness;
+    void led_light(); //apply changes colors or brightness
+};
+
+#endif
